3cff5faa2c90124c69d30dc1d2411aa993309350
Abhishek_Tripathi_VS <abhishek.tripathi@vectoscalar.com>
2024-02-26 14:34:52 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,207 +1,31 @@
-import { useState } from "react";
-import "./styles.css";
+import React, { useState } from 'react'
+import Modal from './Modal'
 
-function handleChange(e, settxt) {
-  settxt(e.target.value);
+const BUTTON_WRAPPER_STYLES = {
+  position: 'relative',
+  zIndex: 1
 }
 
-function handleClick(
-  txt,
-  settxt,
-  settasks,
-  tasks,
-  edited,
-  setedited,
-  completed,
-  setcompleted
-) {
-  if (txt === "") {
-    return;
-  }
-  setcompleted([...completed, false]);
-  settasks([...tasks, txt]);
-  setedited([...edited, false]);
-  settxt("");
-}
-
-function handleDelete(
-  index,
-  settasks,
-  tasks,
-  edited,
-  setedited,
-  completed,
-  setcompleted
-) {
-  const newTasks = [...tasks];
-  const newEdited = [...edited];
-  const newCompleted = [...completed];
-  newCompleted.splice(index, 1);
-  setcompleted(newCompleted);
-  newEdited.splice(index, 1);
-  setedited(newEdited);
-  newTasks.splice(index, 1);
-  settasks(newTasks);
-}
-
-function handleEdit1(edited, setedited, index, settxt2, tasks) {
-  settxt2(tasks[index]);
-  const newEdited = [...edited];
-  newEdited[index] = true;
-  setedited(newEdited);
-}
-
-function handleClick2(txt2, tasks, settasks, index, setedited, edited) {
-  if (txt2 === tasks[index]) {
-    alert("Nothing to change");
-    return;
-  }
-  if (txt2 === "") {
-    alert("Task cannot be empty");
-    return;
-  }
-  const newTasks = [...tasks];
-  newTasks[index] = txt2;
-  settasks(newTasks);
-  const newEdited = [...edited];
-  newEdited[index] = false;
-  setedited(newEdited);
-}
-
-function handleBack(edited, setedited, index) {
-  const newEdited = [...edited];
-  newEdited[index] = false;
-  setedited(newEdited);
-}
-
-function handleComplete(completed, setcompleted, index) {
-  const newCompleted = [...completed];
-  newCompleted[index] = true;
-  setcompleted(newCompleted);
+const OTHER_CONTENT_STYLES = {
+  position: 'relative',
+  zIndex: 2,
+  backgroundColor: 'red',
+  padding: '10px'
 }
 
 export default function App() {
-  const [txt, settxt] = useState("");
-  const [txt2, settxt2] = useState("");
-  const [tasks, settasks] = useState([]);
-  const [edited, setedited] = useState([]);
-  const [completed, setcompleted] = useState([]);
-
+  const [isOpen, setIsOpen] = useState(false)
   return (
-    <div className="App">
-      <input
-        type="text"
-        placeholder="Your Task"
-        value={txt}
-        onChange={(e) => {
-          handleChange(e, settxt);
-        }}
-      />
+    <>
+      <div style={BUTTON_WRAPPER_STYLES} onClick={() => console.log('clicked')}>
+        <button onClick={() => setIsOpen(true)}>Open Modal</button>
 
-      <button
-        onClick={() => {
-          handleClick(
-            txt,
-            settxt,
-            settasks,
-            tasks,
-            edited,
-            setedited,
-            completed,
-            setcompleted
-          );
-        }}
-      >
-        Add
-      </button>
+        <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+          Fancy Modal
+        </Modal>
+      </div>
 
-      <ul>
-        {tasks.map((task, index) => {
-          return (
-            <div className="taskbox">
-              <div key={index}>{task}</div>
-              <button
-                onClick={() => {
-                  handleDelete(
-                    index,
-                    settasks,
-                    tasks,
-                    edited,
-                    setedited,
-                    completed,
-                    setcompleted
-                  );
-                }}
-              >
-                Delete
-              </button>
-
-              {!completed[index] ? (
-                <button
-                  onClick={() => {
-                    handleEdit1(edited, setedited, index, settxt2, tasks);
-                  }}
-                >
-                  Edit
-                </button>
-              ) : (
-                <div></div>
-              )}
-
-              <button
-                onClick={() => {
-                  handleComplete(completed, setcompleted, index);
-                }}
-              >
-                Complete
-              </button>
-              {completed[index] ? (
-                <img
-                  className="imagee"
-                  src="https://www.freepnglogos.com/uploads/tick-png/tick-verde-png-8.png"
-                  alt="completed"
-                />
-              ) : (
-                <div></div>
-              )}
-
-              {edited[index] && !completed[index] ? (
-                <div>
-                  <input
-                    type="text"
-                    Value={tasks[index]}
-                    onChange={(e) => handleChange(e, settxt2)}
-                  />
-                  <button
-                    onClick={() => {
-                      handleClick2(
-                        txt2,
-                        tasks,
-                        settasks,
-                        index,
-                        setedited,
-                        edited
-                      );
-                    }}
-                  >
-                    Save
-                  </button>
-
-                  <button
-                    onClick={() => {
-                      handleBack(edited, setedited, index);
-                    }}
-                  >
-                    Back{" "}
-                  </button>
-                </div>
-              ) : (
-                <div></div>
-              )}
-            </div>
-          );
-        })}
-      </ul>
-    </div>
-  );
+      <div style={OTHER_CONTENT_STYLES}>Other Content</div>
+    </>
+  )
 }
